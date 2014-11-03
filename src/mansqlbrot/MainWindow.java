de7c7d6f835fa72f250116e5e0a567e78c598f52
Gary Briggs <chunky@icculus.org>
2014-11-02 16:15:43 -0800

    Add zoom indicator and iteration control

--- src/mansqlbrot/MainWindow.java
+++ src/mansqlbrot/MainWindow.java
@@ -41,11 +41,11 @@ public class MainWindow extends javax.swing.JFrame {
         displayArea.setFont(new Font("monospaced", Font.PLAIN, 12));
         
         colorInput.setText(defaultColors);
-        calculateCharWidths();
         colors = defaultColors;
         
         try {
             connectDb();
+            reCalculateCharWidths();
             resetDimensions();
             updateDisplay();
         } catch (SQLException | ClassNotFoundException ex) {
@@ -53,11 +53,8 @@ public class MainWindow extends javax.swing.JFrame {
         }
     }
     
-    public void updateFont() {
+    private void reCalculateCharWidths() {
         displayArea.setFont(displayArea.getFont().deriveFont((float)(Integer)charSizeSpinner.getValue()));
-    }
-    
-    private void calculateCharWidths() {
         final FontMetrics fontMetrics = displayArea.getFontMetrics(displayArea.getFont());
         oneCharWidth = (double) fontMetrics.charWidth('M');
         oneCharHeight = (double)fontMetrics.getHeight();
@@ -74,10 +71,12 @@ public class MainWindow extends javax.swing.JFrame {
     Double maxX = 1.2;
     Double minY = -1.0;
     Double maxY = 1.0;
-    Double width = 64.0;
-    Double height = 20.0;
     
-    public void updateDimensions() {
+    Integer defaultIts = 7;
+    
+    Integer currentZoom = 0;
+    
+    public void zoomOnce() {
         Caret c = displayArea.getCaret();
         Point p = c.getMagicCaretPosition();
         
@@ -102,12 +101,11 @@ public class MainWindow extends javax.swing.JFrame {
             System.out.println();
         }
         
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                updateDisplay();
-            }
-        });
-        
+        currentZoom ++;
+        if(0 == currentZoom % 4) {
+            iterationSlider.setValue(iterationSlider.getValue()+1);
+        }
+        zoomSlider.setValue(currentZoom);
     }
     
     public void resetDimensions() {
@@ -116,19 +114,16 @@ public class MainWindow extends javax.swing.JFrame {
         minY = -1.0;
         maxY = 1.0;
         
-        final int prefWidth = displayArea.getWidth();
-        final int prefHeight = displayArea.getHeight();
-        width = (double) (prefWidth / oneCharWidth) - 1.0;
-        height = (double) (prefHeight / oneCharHeight) - 4.0;
-        
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                updateDisplay();
-            }
-        });
+        iterationSlider.setValue(defaultIts);
+        currentZoom = 0;
+        zoomSlider.setValue(currentZoom);
     }
     
     public void updateDisplay() {
+        
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                
         try {
             String sql = "WITH RECURSIVE\n"
                     + "  xaxis(x) AS (VALUES(?) UNION ALL SELECT x+? FROM xaxis WHERE x<?),\n"
@@ -137,27 +132,42 @@ public class MainWindow extends javax.swing.JFrame {
                     + "    SELECT 0, x, y, 0.0, 0.0 FROM xaxis, yaxis\n"
                     + "    UNION ALL\n"
                     + "    SELECT iter+1, cx, cy, x*x-y*y + cx, 2.0*x*y + cy FROM m \n"
-                    + "     WHERE (x*x + y*y) < 4.0 AND iter<28\n"
+                    + "     WHERE (x*x + y*y) < 4.0 AND iter<?\n"
                     + "  ),\n"
                     + "  m2(iter, cx, cy) AS (\n"
                     + "    SELECT max(iter), cx, cy FROM m GROUP BY cx, cy\n"
                     + "  ),\n"
                     + "  a(t) AS (\n"
-                    + "    SELECT group_concat( substr(?, 1+min(iter/7,?), 1), '') \n"
+                    + "    SELECT group_concat( substr(?, 1+min(iter/?,?), 1), '') \n"
                     + "    FROM m2 GROUP BY cy\n"
                     + "  )\n"
                     + "SELECT group_concat(rtrim(t),x'0a') FROM a";
          
+            final int prefWidth = displayArea.getWidth();
+            final int prefHeight = displayArea.getHeight();
+            Double width = (double) (prefWidth / oneCharWidth) - 1.0;
+            Double height = (double) (prefHeight / oneCharHeight) - 4.0;
+            
             String colors = colorInput.getText();
+            
+            Integer its = (iterationSlider.getValue());
+            Integer iterations = (colors.length()-1)*its;
             try (PreparedStatement stmt = conn.prepareStatement(sql)) {
                 stmt.setDouble(1, minX);
                 stmt.setDouble(2, (maxX - minX) / width);
                 stmt.setDouble(3, maxX);
+                
                 stmt.setDouble(4, minY);
                 stmt.setDouble(5, (maxY - minY) / height);
                 stmt.setDouble(6, maxY);
-                stmt.setString(7, colors);
-                stmt.setInt(8, colors.length());
+                
+                stmt.setDouble(7, iterations);
+                
+                stmt.setString(8, colors);
+                stmt.setInt(9, its);
+                stmt.setInt(10, colors.length());
+//                stmt.setDouble(10, iterations);
+//                stmt.setInt(11, colors.length());
 
                 try (ResultSet rs = stmt.executeQuery()) {
                     StringBuilder sb = new StringBuilder();
@@ -172,6 +182,9 @@ public class MainWindow extends javax.swing.JFrame {
             ex.printStackTrace();
         }
         displayArea.setText(currentRender);
+        
+            }
+        });
     }
 
     public void connectDb() throws ClassNotFoundException, SQLException {
@@ -190,11 +203,15 @@ public class MainWindow extends javax.swing.JFrame {
 
         jScrollPane1 = new javax.swing.JScrollPane();
         displayArea = new javax.swing.JTextArea();
+        zoomSlider = new javax.swing.JSlider();
+        jPanel1 = new javax.swing.JPanel();
         jButton1 = new javax.swing.JButton();
         jLabel2 = new javax.swing.JLabel();
         colorInput = new javax.swing.JTextField();
         jLabel1 = new javax.swing.JLabel();
         charSizeSpinner = new javax.swing.JSpinner();
+        jLabel3 = new javax.swing.JLabel();
+        iterationSlider = new javax.swing.JSlider();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("ManSQLBrot");
@@ -217,11 +234,24 @@ public class MainWindow extends javax.swing.JFrame {
         jScrollPane1.setViewportView(displayArea);
 
         gridBagConstraints = new java.awt.GridBagConstraints();
-        gridBagConstraints.gridwidth = java.awt.GridBagConstraints.REMAINDER;
+        gridBagConstraints.gridwidth = 7;
         gridBagConstraints.fill = java.awt.GridBagConstraints.BOTH;
         gridBagConstraints.weightx = 1.0;
         gridBagConstraints.weighty = 1.0;
         getContentPane().add(jScrollPane1, gridBagConstraints);
+
+        zoomSlider.setMaximum(25);
+        zoomSlider.setOrientation(javax.swing.JSlider.VERTICAL);
+        zoomSlider.setToolTipText("");
+        zoomSlider.setValue(0);
+        zoomSlider.setEnabled(false);
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridwidth = java.awt.GridBagConstraints.REMAINDER;
+        gridBagConstraints.fill = java.awt.GridBagConstraints.VERTICAL;
+        gridBagConstraints.weighty = 1.0;
+        getContentPane().add(zoomSlider, gridBagConstraints);
+
+        jPanel1.setLayout(new java.awt.GridBagLayout());
 
         jButton1.setText("Reset Zoom");
         jButton1.addActionListener(new java.awt.event.ActionListener() {
@@ -231,23 +261,23 @@ public class MainWindow extends javax.swing.JFrame {
         });
         gridBagConstraints = new java.awt.GridBagConstraints();
         gridBagConstraints.weightx = 1.0;
-        getContentPane().add(jButton1, gridBagConstraints);
+        jPanel1.add(jButton1, gridBagConstraints);
 
         jLabel2.setText("\"Colors\":");
         gridBagConstraints = new java.awt.GridBagConstraints();
         gridBagConstraints.anchor = java.awt.GridBagConstraints.LINE_END;
-        getContentPane().add(jLabel2, gridBagConstraints);
+        jPanel1.add(jLabel2, gridBagConstraints);
 
         colorInput.setText("jTextField1");
         gridBagConstraints = new java.awt.GridBagConstraints();
         gridBagConstraints.anchor = java.awt.GridBagConstraints.LINE_START;
         gridBagConstraints.weightx = 1.0;
-        getContentPane().add(colorInput, gridBagConstraints);
+        jPanel1.add(colorInput, gridBagConstraints);
 
         jLabel1.setText("Font Size:");
         gridBagConstraints = new java.awt.GridBagConstraints();
         gridBagConstraints.anchor = java.awt.GridBagConstraints.LINE_END;
-        getContentPane().add(jLabel1, gridBagConstraints);
+        jPanel1.add(jLabel1, gridBagConstraints);
 
         charSizeSpinner.setModel(new javax.swing.SpinnerNumberModel(12, 1, 20, 1));
         charSizeSpinner.addChangeListener(new javax.swing.event.ChangeListener() {
@@ -258,28 +288,57 @@ public class MainWindow extends javax.swing.JFrame {
         gridBagConstraints = new java.awt.GridBagConstraints();
         gridBagConstraints.anchor = java.awt.GridBagConstraints.LINE_START;
         gridBagConstraints.weightx = 1.0;
-        getContentPane().add(charSizeSpinner, gridBagConstraints);
+        jPanel1.add(charSizeSpinner, gridBagConstraints);
+
+        jLabel3.setText("Iterations:");
+        jPanel1.add(jLabel3, new java.awt.GridBagConstraints());
+
+        iterationSlider.setMaximum(20);
+        iterationSlider.setMinimum(1);
+        iterationSlider.setSnapToTicks(true);
+        iterationSlider.setToolTipText("");
+        iterationSlider.setValue(7);
+        iterationSlider.addChangeListener(new javax.swing.event.ChangeListener() {
+            public void stateChanged(javax.swing.event.ChangeEvent evt) {
+                iterationSliderStateChanged(evt);
+            }
+        });
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridwidth = java.awt.GridBagConstraints.REMAINDER;
+        gridBagConstraints.fill = java.awt.GridBagConstraints.VERTICAL;
+        jPanel1.add(iterationSlider, gridBagConstraints);
+
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridwidth = java.awt.GridBagConstraints.REMAINDER;
+        gridBagConstraints.fill = java.awt.GridBagConstraints.HORIZONTAL;
+        getContentPane().add(jPanel1, gridBagConstraints);
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
     private void jScrollPane1ComponentResized(java.awt.event.ComponentEvent evt) {//GEN-FIRST:event_jScrollPane1ComponentResized
-        updateDimensions();
+        resetDimensions();
+        updateDisplay();
     }//GEN-LAST:event_jScrollPane1ComponentResized
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         resetDimensions();
+        updateDisplay();
     }//GEN-LAST:event_jButton1ActionPerformed
 
     private void displayAreaMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_displayAreaMouseClicked
-        updateDimensions();
+        zoomOnce();
+        updateDisplay();
     }//GEN-LAST:event_displayAreaMouseClicked
 
     private void charSizeSpinnerStateChanged(javax.swing.event.ChangeEvent evt) {//GEN-FIRST:event_charSizeSpinnerStateChanged
-        updateFont();
-        calculateCharWidths();
-        updateDimensions();
+        reCalculateCharWidths();
+        updateDisplay();
     }//GEN-LAST:event_charSizeSpinnerStateChanged
+
+    private void iterationSliderStateChanged(javax.swing.event.ChangeEvent evt) {//GEN-FIRST:event_iterationSliderStateChanged
+        updateDisplay();
+    }//GEN-LAST:event_iterationSliderStateChanged
 
     /**
      * @param args the command line arguments
@@ -320,9 +379,13 @@ public class MainWindow extends javax.swing.JFrame {
     private javax.swing.JSpinner charSizeSpinner;
     private javax.swing.JTextField colorInput;
     private javax.swing.JTextArea displayArea;
+    private javax.swing.JSlider iterationSlider;
     private javax.swing.JButton jButton1;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JPanel jPanel1;
     private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JSlider zoomSlider;
     // End of variables declaration//GEN-END:variables
 }
